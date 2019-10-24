1dae10ccb93a5a99a483fd147fb63d9e910da9d4
kaapomoi <kaapomo@gmail.com>
2019-10-24 15:30:52 +0300

    Added a lot of variables and comments on what they should do

--- Assets/Scripts/Deer.cs
+++ Assets/Scripts/Deer.cs
@@ -5,16 +5,38 @@ using UnityEngine;
 public class Deer : Creature
 {
 
-    // what is this v
-    public Deer(float _hunger, float _thirst, float _breeding_urge, float _max_speed, float _energy, float _sight_radius, float _determinedness)
-        : base(_hunger, _thirst, _breeding_urge, _max_speed, _energy, _sight_radius, _determinedness)
-    { }
+    /// <summary>
+    /// Create a Deer with these parametres:
+    /// </summary>
+    /// <param name="_age">Set to 0 please, increases as time passes</param>
+    /// <param name="_hunger">Set to 0 please, increases as time passes + inverse energy, goes to 0 after eating</param>
+    /// <param name="_thirst">Set to 0 please, increases as time passes + inverse energy, goes to 0 after drinking</param>
+    /// <param name="_reproduction_urge">Set to 0 please, increases as time passes, goes to 0 after reproducing</param>
+    /// <param name="_max_speed">0-1, goes down as age increases</param>
+    /// <param name="_energy">set to 1 please, decreases as time passes, increases while idling/resting</param>
+    /// <param name="_sight_radius">set to 10-50??, decreases as age increases</param>
+    /// <param name="_determinedness">set to 0-1, how much does the creature change direction while wandering</param>
+    /// <param name="_diet_type">0: vegetarian</param>
+    /// <param name="_is_male">50-50 is the creature a male when born</param>
 
+    public Deer(float _age, float _hunger, float _thirst, float _reproduction_urge, float _max_speed, float _energy, int _sight_radius, float _determinedness, int _diet_type, bool _is_male)
+    {
+        Age = _age;
+        Hunger = _hunger;
+        Thirst = _thirst;
+        ReproductionUrge = _reproduction_urge;
+        MaxSpeed = _max_speed;
+        Energy = _energy;
+        SightRadius = _sight_radius;
+        Determinedness = _determinedness;
+        DietType = _diet_type;
+        IsMale = _is_male;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        energy = 512;
+
     }
 
     // Update is called once per frame
@@ -23,21 +45,23 @@ public class Deer : Creature
         float dt = Time.deltaTime;
 
         // what even is this v
-        hunger += (0.001f - (energy / 2048) / 1000) * dt;
-        thirst += 0.0015f * dt;
-        energy -= 0.1f * dt;
+        Age += 0.1f * dt;
+        Hunger += (0.001f - (Energy / 2048)) * dt;
+        Thirst += 0.0015f * dt;
+        Energy -= 0.020f * dt;
+        ReproductionUrge += 0.001f * dt;
 
-        hunger = Mathf.Clamp01(hunger);
-        thirst = Mathf.Clamp01(thirst);
+        Hunger = Mathf.Clamp01(Hunger);
+        Thirst = Mathf.Clamp01(Thirst);
+        Energy = Mathf.Clamp01(Energy);
+        ReproductionUrge = Mathf.Clamp01(ReproductionUrge);
 
-        if (thirst >= 1.0f || hunger >= 1.0f)
+        if (Thirst >= 1.0f || Hunger >= 1.0f)
         {
             Die();
         }
 
-        Wander();
-
-
+        BehaviourTree(0);
 
     }
 
